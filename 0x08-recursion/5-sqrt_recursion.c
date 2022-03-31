303bbeb8c2f8961bbf4f3d37002ca76599092d0c
kogi-b <briankogi18@example.com>
2022-03-30 18:10:28 -0700

    second change

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,45 +1,50 @@
+#include "holberton.h"
+
 /**
- * _sqrt_recursion_wrapper - a wrapper that does the recursion bit
- *
- * @n : input number
- * @min: minimum number to guess
- * @max: maximum number to guess
- *
- * Return: square root of @n or -1
-*/
-int _sqrt_recursion_wrapper(int n, int min, int max)
+ * _checker - Checks the function _sqrt_recursion
+ * @a: Integer
+ * @b: Integer
+ * Return: A checker for the above function
+ */
+
+int _checker(int a, int b)
 {
-	int guess, guess_squared;
+if (b == 0 || b == 1)
+{
+return (b);
+}
 
-	guess = (min + max) / 2;		/*get guess (g)*/
-	guess_squared = guess * guess;		/*square guess (sg)*/
+else if ((a * a) < b)
+{
+return (_checker(a + 1, b));
+}
+else if ((a * a) == b)
+{
+return (a);
+}
+return (-1);
 
-	if (guess_squared == n)			/*if squared guess == n return guess*/
-		return (guess);
-	else if (min == max)			/*this means n doesn't have a perfect square*/
-		return (-1);
-	else if (guess_squared < n)		/*overshoot g if sg is less than the n*/
-		return (_sqrt_recursion_wrapper(n, guess + 1, max));
-	else					/*undershoot g if sg is more than the n*/
-		return (_sqrt_recursion_wrapper(n, min, guess - 1));
 }
 
 /**
- * _sqrt_recursion - a function that returns the natural
- *                   square root of a number
- *
- * @n: input number
- *
- * Return: square root
-*/
+ *_sqrt_recursion - evaluates a square root
+ *@n: interger to be calculated
+ *Return: The square root of an integer
+ */
+
 int _sqrt_recursion(int n)
 {
-	if (n == 1)		/*sqrt(1) == 1*/
-		return (1);
-	else if (n == 0)	/*sqrt(0) == 0*/
-		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
-		return (-1);
-	else
-		return (_sqrt_recursion_wrapper(n, 1, n));
+int x;
+
+x = 0;
+if (x < 0)
+{
+return (-1);
 }
+else
+{
+return (_checker(x, n));
+}
+
+}
+
